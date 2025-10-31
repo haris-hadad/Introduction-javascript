2e5587cd13302a3361fc6e59643bede4d8eb786a
haris-hadad <haris.hadad@laplateforme.io>
2025-10-31 11:59:16 +0100

    Afficher du text dans la console web lorsqu'on interragi avec un bouton

---
+++ jour02/job01/index.php
@@ -0,0 +1,12 @@
+<DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Js++</title>
+</head>
+<body>
+    <article id="citation">La vie a beaucoup plus d’imagination que nous</article>
+    <button id="btn">cliquer ici</button>
+    <script src="script.js"></script>
+</body>
+</html>
---
+++ jour02/job01/script.js
@@ -0,0 +1,9 @@
+const button = document.getElementById('btn');
+ const article = document.getElementById('citation');
+    button.addEventListener('click', () => {        
+    console.log(article.textContent);
+    });
+
+
+
+
